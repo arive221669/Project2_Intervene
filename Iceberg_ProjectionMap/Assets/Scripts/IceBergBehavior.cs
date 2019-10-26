aa5d632e9f35b1cf2a436721ade957123573b2fa
Anthony <anthony.rivera3@mail.citytech.cuny.edu>
2019-10-26 11:40:27 -0400

    Added materials and building tests, added shrink feature

---
+++ Iceberg_ProjectionMap/Assets/Scripts/IceBergBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceBergBehavior : MonoBehaviour
+{
+    private float timer;
+    public GameObject IceBerg;
+
+    private float minSize = 0.1f;
+
+    private float shrinkSpeed = -0.1f;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        Debug.Log("Time past: " + timer);
+
+        if(timer >= 5)
+        {
+            IceBerg.transform.localScale += new Vector3(10, 5, 10) * shrinkSpeed * Time.deltaTime;
+
+/*            if (IceBerg.transform.localScale < minSize)
+            {
+
+            }*/
+        }
+        
+    }
+}
