300531ec10a40aa8fbd31edf4856dea934e98d25
Anthony <anthony.rivera3@mail.citytech.cuny.edu>
2019-11-04 23:48:17 -0500

    New Project has been added

---
+++ TargetAd/Assets/Scripts/TargetMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMovement : MonoBehaviour
+{
+    private int rotateX = 2;
+    private int rotateY = 1;
+    private int rotateZ = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotateY, 0);
+
+    }
+}
